11e2316fd65bc297aeb5291566cad08c76a62a55
Lilia Khazieva <lilia002777@mail.ru>
2023-03-14 22:09:59 +0500

    pravki alt

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,7 +5,7 @@ function ImagePopup({ isOpen, card, onClose }) {
     <div className={`popup popup_big-image ${isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__wrapper">
         <button type="button" className="popup__close popup__close_big-image" onClick={onClose}></button>
-        <img className="popup__image" src={card ? card.link : ""} alt="изображение"/>
+        <img className="popup__image" src={card ? card.link : ""} alt={card ? card.name : ""}/>
           <p className="popup__title-image">{card ? card.name : ""}</p>
       </div>
     </div>
